75f67a8aaee7b7f5b8c5c66b50fc112a77da0be1
Michał <michalkapustka3@gmail.com>
2024-04-15 20:20:00 +0200

    3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Pobranie listy składników
+const ingredientsList = document.getElementById("ingredients");
+
+// Utworzenie elementów <li> dla każdego składnika
+const liElements = ingredients.map((ingredient) => {
+  // Utworzenie nowego elementu <li>
+  const li = document.createElement("li");
+  // Ustawienie zawartości tekstowej na nazwę składnika
+  li.textContent = ingredient;
+  // Dodanie klasy "item" do elementu <li>
+  li.classList.add("item");
+  // Zwrócenie utworzonego elementu <li>
+  return li;
+});
+
+// Wstawienie wszystkich elementów <li> do listy ul#ingredients
+ingredientsList.append(...liElements);
